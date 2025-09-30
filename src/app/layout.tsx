08b8d282102acabb30c63dd52482cd7c9c1d51f5
Abhishek Sharma <i.am.racer.s@gmail.com>
2025-09-30 08:13:45 +0000

    the website is not flexible it is not covering the whole screen  (_for e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <SidebarProvider>
-            <div className="flex">
+            <div className="flex flex-1">
               <AppSidebar />
               <AppContent>
                 {children}
