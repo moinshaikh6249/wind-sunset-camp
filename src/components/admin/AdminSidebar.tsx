e70f49dadca83d841fac7016abc8ffddfd1699ad
Abhishek Sharma <i.am.racer.s@gmail.com>
2025-10-09 15:01:36 +0000

    Make them same as logo and title (_for element div_)

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -7,6 +7,7 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { Logo } from "../Logo";
 
 
 const navLinks = [
@@ -29,10 +30,7 @@ export function AdminSidebar() {
         <div className="hidden border-r bg-muted/40 md:block">
             <div className="flex h-full max-h-screen flex-col gap-2">
                 <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-                    <Link href="/" className="flex items-center gap-2 font-semibold">
-                        <Tent className="h-6 w-6" />
-                        <span className="">Wind & Sunset</span>
-                    </Link>
+                    <Logo />
                 </div>
                 <div className="flex-1">
                     <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
