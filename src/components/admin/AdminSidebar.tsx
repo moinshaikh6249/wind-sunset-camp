1948ee268021a675d66b941570548e17a4ab0248
Abhishek Sharma <i.am.racer.s@gmail.com>
2025-10-09 14:57:41 +0000

    remove this (_for element div_)

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Link from "next/link";
@@ -34,10 +33,6 @@ export function AdminSidebar() {
                         <Tent className="h-6 w-6" />
                         <span className="">Wind & Sunset</span>
                     </Link>
-                    <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-                        <Bell className="h-4 w-4" />
-                        <span className="sr-only">Toggle notifications</span>
-                    </Button>
                 </div>
                 <div className="flex-1">
                     <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
