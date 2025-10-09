3466427bc196937de782713cc2be4e75eff131d2
Abhishek Sharma <i.am.racer.s@gmail.com>
2025-10-09 14:50:17 +0000

    remove this  (_for element <CardHeader>_)

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import Link from "next/link";
@@ -56,20 +57,7 @@ export function AdminSidebar() {
                     </nav>
                 </div>
                 <div className="mt-auto p-4">
-                    <Card x-chunk="dashboard-02-chunk-0">
-                        <CardHeader className="p-2 pt-0 md:p-4">
-                            <CardTitle>Upgrade to Pro</CardTitle>
-                            <CardDescription>
-                                Unlock all features and get unlimited access to our support
-                                team.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                            <Button size="sm" className="w-full">
-                                Upgrade
-                            </Button>
-                        </CardContent>
-                    </Card>
+                    
                 </div>
             </div>
         </div>
